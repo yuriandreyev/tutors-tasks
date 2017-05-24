188a5d0edb3269ae2f5f10e83ebe8bfdfe632e18
Yuri <ya_andreev@mail.ru>
2017-05-24 21:08:15 +0600

    Comments to functions have been added

--- pitable.py
+++ pitable.py
@@ -1,15 +1,18 @@
-'''This module reads the number from file and writes Pifagor's table
-of number to the same file. Script gets file's name from command line'''
+''' This module reads the number from file and writes Pifagor's table
+of number to the same file. Script gets file's name from command line '''
 
 import sys
 
 def read_number(file_name):
+    ''' Function takes file's name as argument, reads number from the file
+    and returns it as integer '''
     with open(file_name) as myfile:
         number = myfile.readline()
         number.rstrip()
     return int(number)
 
 def calc_table(number):
+    ''' Function takes a number as argument and calculates Pifagor's table of the number '''
     raw = col = list(range(1,number+1))
     result_table = []
     
@@ -21,6 +24,8 @@ def calc_table(number):
     return result_table
 
 def output_result(file_name):
+    ''' Function takes file's name as arg, read a number from the file and
+    writes to the file Pifagor's table of the number '''
     number = read_number(file_name)
     result = calc_table(number)
     with open('numbers.txt', 'w') as myfile:

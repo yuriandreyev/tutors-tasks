e6a5f5c4f62d208314bdb4aeb54f68480af389bd
Yuri <ya_andreev@mail.ru>
2017-05-25 18:32:05 +0600

    code style changed

--- 3x0.py
+++ 3x0.py
@@ -1,7 +1,12 @@
 import re
 
-text = 'horse 300 power. 380 volt'
-template = re.compile(' 3\d0 ')
-res = template.findall(text)
 
-print(res)
+def find_3digit_numbers(file_name):
+    ''' Function finds and returns three-digit numbers which start at 3 and ends by 0 '''
+    with open(file_name) as my_file:
+        text = my_file.read()
+    template = re.compile(' 3\d0 ')
+    res = template.findall(text)
+    return res
+
+print(find_3digit_numbers('text3x0.txt'))
--- find_numbers.py
+++ find_numbers.py
@@ -4,6 +4,6 @@ import re
 
 text = """If 300 spartans were so brave, so 500 spartans could destroy more than 10k warriors of Darius,
        but 15k and even 20k."""
-template = re.compile('\d+k|\d+m|\d+')
+template = re.compile('\d+')
 res = template.findall(text)
 print(res)
